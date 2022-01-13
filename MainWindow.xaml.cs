5a82252132c8da66dc77e9db8333b3ee866b364a
Abbosbek Niyozqulov <abbos6882@gmail.com>
2022-01-13 23:03:49 +0500

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,21 +9,12 @@ using OfficeHelper.Models;
 
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
-using System.Windows.Documents;
-using Forms = System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Controls;
 using System.Collections.ObjectModel;
 
